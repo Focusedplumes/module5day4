d2577a13521412f17ead997143dceb3257237886
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-06-29 13:09:39 -0500

    update code

--- exercisecoffee-shop/coffee-shop/src/pages/api/cart/index.js
+++ exercisecoffee-shop/coffee-shop/src/pages/api/cart/index.js
@@ -21,9 +21,11 @@ export default function handler(req, res) {
       
     }
 
+    
+
     // TODO #1 use the db.cart.getById method to see if we already have the item in the cart
     // IF we do have the item in the cart already, then update the quantity, otherwise add the item
-    
+        
     db.cart.add(req.body);
     res.status(200).json({ message: "Item added to cart" });
   } else {
