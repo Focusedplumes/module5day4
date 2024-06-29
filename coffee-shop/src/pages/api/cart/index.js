3ed14444bff1dc54e3938c21f26cf8c62dee29a7
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-06-28 23:57:16 -0500

    updating

--- coffee-shop/src/pages/api/cart/index.js
+++ coffee-shop/src/pages/api/cart/index.js
@@ -1,15 +1,13 @@
 import { db } from "@/server/db";
 
 export default function handler(req, res) {
-    const menuItems = db.cart.get() //mock version of our database
-    if (req.method === 'GET') {
-        const cartItems = db.cart.get();
-        res.status(200).json({ cart: cartItems }); // when someone hits this end point, then responding with an object that has a menu key and the menu key will have the array of objects menuItems
-    } else if (req.method === 'POST') {
-      db.cart.add(req.body);
-      req.status(200).json({ message: "Item added to cart"});
-        } else {
-        res.status(404).json({ message: "We only support GET requests" });
-    }
-    
-  } //for the function is just meaning "request handler"
+  if (req.method === "GET") {
+    const cartItems = db.cart.get();
+    res.status(200).json({ cart: cartItems });
+  } else if (req.method === "POST") {
+    db.cart.add(req.body);
+    res.status(200).json({ message: "Item added to cart" });
+  } else {
+    res.status(404).json({ message: "We only support GET requests" });
+  }
+}
