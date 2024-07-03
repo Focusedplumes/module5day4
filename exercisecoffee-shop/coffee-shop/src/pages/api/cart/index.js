823c97067776acb1cd2fa1ad001139633d85dc63
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-07-02 22:04:11 -0500

    working through to do 1

--- exercisecoffee-shop/coffee-shop/src/pages/api/cart/index.js
+++ exercisecoffee-shop/coffee-shop/src/pages/api/cart/index.js
@@ -21,7 +21,18 @@ export default function handler(req, res) {
       
     }
 
-    
+   if (req.method === "PUT") {
+    const { foundItem, newQuantity } = req.body
+    const test = db.cart.getById(id)
+    console.log(test)
+    res.status(200).json({cart: cartItem});
+   
+   } if (foundItem) {
+    db.cart.updateById(foundItem, newQuantity)
+   } else {
+    db.cart.add(id)
+   }
+   
 
     // TODO #1 use the db.cart.getById method to see if we already have the item in the cart
     // IF we do have the item in the cart already, then update the quantity, otherwise add the item
